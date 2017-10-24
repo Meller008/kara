1556759c32c6f1b671990fb2249b256274f0bca8
Meller008 <cs007@mail.ru>
2017-10-24 19:20:47 +0300

    тест Гит

--- form/main_window.py
+++ form/main_window.py
@@ -2,7 +2,7 @@ from os import getcwd
 from PyQt5.QtWidgets import QMainWindow, QMdiSubWindow, QLabel
 from PyQt5.uic import loadUiType
 from PyQt5.QtGui import QIcon, QBrush, QImage
-import sys.getsizeof()
+import sys
 
 main_class = loadUiType(getcwd() + '/ui/main_window.ui')[0]
 
